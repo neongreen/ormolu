01f8c79c23e402d44c68d07cb09c98d588cc9a89
Artyom Kazak <yom@artyom.me>
2020-01-11 01:15:50 +0300

    More examples

---
+++ data/examples/declaration/class/newlines-and-default-decls-out.hs
@@ -0,0 +1,12 @@
+class Foo a where
+  foo :: a
+  default foo :: ()
+  foo = ()
+  bar :: a
+  default bar :: ()
+  bar = ()
+
+  qux :: a
+
+  default qux :: ()
+  qux = ()
---
+++ data/examples/declaration/class/newlines-and-haddocks.hs
@@ -0,0 +1,15 @@
+class Foo a where
+  -- | Haddock
+  foo :: a
+  -- | Another Haddock
+  bar :: a
+
+  -- | One more Haddock
+  qux :: a
+  -- Comment before a Haddock
+  -- | And one more Haddock
+  xyz :: a
+
+  -- | Haddock followed by a blank line
+
+  abc :: a
